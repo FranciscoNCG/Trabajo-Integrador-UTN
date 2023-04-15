471e106ca798becd5b2fd9d10a73f23911079e8a
FranciscoNCG <68757773+FranciscoNCG@users.noreply.github.com>
2023-04-15 04:10:07 -0300

    galeria

--- JS/galeria.js
+++ JS/galeria.js
@@ -2,15 +2,14 @@ const imagenes = document.querySelectorAll('.img-galeria');
 let imagenCompleta;
 let indiceActual;
 
+        // Elemento de imagen en tamaño completo
 imagenes.forEach((imagen, indice) => {
     imagen.addEventListener('click', () => {
-        // Elemento de imagen en tamaño completo
         imagenCompleta = document.createElement('img');
         imagenCompleta.src = imagen.src;
         imagenCompleta.alt = imagen.alt;
         imagenCompleta.classList.add('img-completa');
 
-        // Botones de anterior y siguiente
         const btnAnterior = document.createElement('button');
         btnAnterior.innerHTML = '<i class="bx bx-chevron-left"></i>';
         btnAnterior.classList.add('btn-anterior');
@@ -19,7 +18,7 @@ imagenes.forEach((imagen, indice) => {
         btnSiguiente.innerHTML = '<i class="bx bx-chevron-right"></i>';
         btnSiguiente.classList.add('btn-siguiente');
 
-        // Agrega eventos a botones de anterior y siguiente
+
         btnAnterior.addEventListener('click', () => {
             indiceActual--;
             if (indiceActual < 0) {
@@ -32,9 +31,9 @@ imagenes.forEach((imagen, indice) => {
         btnSiguiente.addEventListener('click', () => {
             indiceActual++;
             if (indiceActual >= imagenes.length) {
-                /*Verifica si es la última imagen */
+
                 if (indiceActual === imagenes.length) {
-                /* Quita la imagen */
+
                     document.body.removeChild(imagenCompleta);
                     document.body.removeChild(btnAnterior);
                     document.body.removeChild(btnSiguiente);
@@ -45,12 +44,12 @@ imagenes.forEach((imagen, indice) => {
             imagenCompleta.alt = imagenes[indiceActual].alt;
         });
 
-        // Agrega la imagen completa y los botones a la página
+
         document.body.appendChild(imagenCompleta);
         document.body.appendChild(btnAnterior);
         document.body.appendChild(btnSiguiente);
 
-        // Guarda el índice actual
+
         indiceActual = indice;
     });
 });
